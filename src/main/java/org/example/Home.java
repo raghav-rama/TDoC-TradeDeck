2ef8e590e14832dd4e682a7aef446b96c1ce88e2
raghav-rama <hackerer528@gmail.com>
2022-12-16 20:57:29 +0530

    feat: just testing apis

--- src/main/java/org/example/Home.java
+++ src/main/java/org/example/Home.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Set;
+
+import org.json.*;
 
 public class Home {
   private JButton button1;
@@ -42,12 +49,34 @@ public class Home {
     }
   });
 }
-  public static void main (String[] args) {
+
+  public static void fetch() throws IOException, InterruptedException {
+    String url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=XYMD6F46NUUEPLX0";
+    HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+    HttpClient client = HttpClient.newBuilder().build();
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    String info = response.body();
+    JSONObject obj = new JSONObject(info);
+    System.out.println("obj: "+obj);
+    JSONObject time = obj.getJSONObject("Time Series (5min)");
+    System.out.println("time: "+time);
+    Set<String> keys = time.keySet();
+    String[] dates = keys.toArray(new String[keys.size()]);
+    Arrays.sort(dates);
+    System.out.println("dates: "+ Arrays.toString(dates));
+    String[] close = new String[keys.size()];
+    for(int i=0;i<close.length;++i)
+      close[i] = time.getJSONObject(dates[i]).getString("4. close");
+    System.out.println("closing val: "+Arrays.toString(close));
+  }
+
+  public static void main (String[] args) throws IOException, InterruptedException {
     JFrame frame = new JFrame("Home");
     frame.setContentPane(new Home().panelMain);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
     frame.setVisible(true);
+    fetch();
   }
 
   private void createUIComponents() {
